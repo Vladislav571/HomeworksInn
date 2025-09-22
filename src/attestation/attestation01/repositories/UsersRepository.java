e4ffa5afb65a23aa4db0f3cb540875ab3317e151
Vladislav571 <agaeff.vlad@mail.ru>
2025-09-22 18:53:30 +0300

    attestation01

---
+++ src/attestation/attestation01/repositories/UsersRepository.java
@@ -0,0 +1,15 @@
+package attestation.attestation01.repositories;
+
+import attestation.attestation01.model.User;
+
+import java.util.List;
+
+public interface UsersRepository {
+
+    void create(User user);
+    User findById(String id);
+    List<User> findAll();
+    void update(User user);
+    void deleteById(String id);
+    void deleteAll();
+}
